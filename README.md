ef0fc102137cc7a23e305aecc3b3fe134c5586d7
uller91 <girnikis@gmail.com>
2025-03-09 09:19:17 -0700

    The current final version with more small changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# mazeSolv
+# mazeSolver
 Maze Solver in Python3
+It utilizes tkinter for drawing the the maze and DFS algorythm to solve it.
--- main.py
+++ main.py
@@ -6,14 +6,19 @@ from maze import Maze
             
 
 def main():
-    #print("Hello world!")
-
     #window created
     win = Window(800, 600)
 
     #drawing
-    maze = Maze(Point(100, 100), 12, 8, 50, 50, win, 0) # seed = 0 - a good seed
-    maze.solve(0, 0)
+    maze = Maze(Point(100, 100), 12, 8, 50, 50, win) # seed = 0 - a good seed
+    
+    #solving the maze
+    result = maze.solve(0, 0)
+    if result == True:
+        print("The mase is solved!")
+    else:
+        print("The soluition for this maze doesn't exist!")
+
 
     #waiting for the window to close
     win.wait_for_close()
